adfbab878083b3a0028a967b9e9a99ea1427014f
Kevin Nolan <kevin.nolan@nearform.com>
2023-02-22 13:04:24 +0000

    feat: add angular material for basic styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule, MatIconModule, MatButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatSlideToggleModule, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
