b74adb176f71723ad4b20899a889212b2e695388
Kevin Nolan <kevin.nolan@nearform.com>
2023-02-24 17:23:05 +0000

    feat: change environment setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl: 'https://api.themoviedb.org/',
   imageBaseUrl: 'https://image.tmdb.org/t/p/',
-  readonlyAccessToken: 'REPLACE_TOKEN_HERE',
+  readonlyAccessToken: '<YOUR_TOKEN_HERE>',
   authRedirectUrl: 'https://www.themoviedb.org/auth/access'
 }
