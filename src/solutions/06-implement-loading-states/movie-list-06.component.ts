bb07a001b2fdff3c511255835db8a7dcc0aa9899
Kevin Nolan <kevin.nolan@nearform.com>
2023-02-27 17:30:06 +0000

    feat: solutions 6

--- src/solutions/06-implement-loading-states/movie-list-06.component.ts
+++ src/solutions/06-implement-loading-states/movie-list-06.component.ts
@@ -1,9 +1,51 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { MovieService } from './movie.service'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+
+// TODO: doesn't seem to show spinner even when on slow network
 @Component({
   selector: 'app-movie-list-06-component',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>Solution 6</p>`
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatButtonModule
+  ],
+  template: `
+    <ng-container *ngIf="movies$ | async as movies">
+      <div *ngIf="movies.isLoading" class="centered-content height-page">
+        <mat-progress-spinner
+          color="primary"
+          mode="indeterminate"
+          diameter="50"
+        ></mat-progress-spinner>
+      </div>
+      <ng-container *ngIf="!movies.isLoading">
+        <div class="stack">
+          <mat-card *ngFor="let movie of movies.data">
+            <mat-card-header>
+              <mat-card-title>{{ movie.title }}</mat-card-title>
+            </mat-card-header>
+            <mat-card-content>
+              <div class="flex">
+                <img [src]="movie.poster" [alt]="movie.title" />
+                <p>
+                  {{ movie.description }}
+                </p>
+              </div>
+            </mat-card-content>
+          </mat-card>
+        </div>
+      </ng-container>
+    </ng-container>
+  `
 })
-export class MovieList06Component {}
+export class MovieList06Component {
+  movies$ = this.moviesService.getPopular()
+
+  constructor(private moviesService: MovieService) {}
+}
--- src/solutions/06-implement-loading-states/movie.service.ts
+++ src/solutions/06-implement-loading-states/movie.service.ts
@@ -1,6 +1,36 @@
 import { Injectable } from '@angular/core'
+import { PagedApi } from '../../app/types/paged.types'
+import {
+  MovieSummary,
+  MovieSummaryApi
+} from '../../app/types/movie-summary.types'
+import { map, startWith } from 'rxjs'
+import { AsyncState } from '../../app/types/async-state.types'
+import { ApiService } from '../../app/services/api.service'
+import { tmdbPosterThumbnailUrl } from '../../app/utils/tmdb-poster-thumbnail-url'
 
 @Injectable({
   providedIn: 'root'
 })
-export class MovieService {}
+export class MovieService {
+  constructor(private api: ApiService) {}
+
+  getPopular() {
+    return this.api
+      .get<PagedApi<MovieSummaryApi>>({ url: 'movie/popular' })
+      .pipe(
+        map(
+          (data): AsyncState<MovieSummary[]> => ({
+            isLoading: false,
+            data: data.results.map(movie => ({
+              id: movie.id,
+              title: movie.title,
+              description: movie.overview,
+              poster: tmdbPosterThumbnailUrl(movie.poster_path)
+            }))
+          })
+        ),
+        startWith<AsyncState<MovieSummary[]>>({ isLoading: true })
+      )
+  }
+}
