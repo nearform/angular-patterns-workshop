e879d64109de90584913e95be6935c852f1f3526
Ian Lovell <ian.lovell@nearform.com>
2023-03-03 14:12:28 +0000

    wip: update environment

--- apps/movies/src/environments/environment.ts
+++ apps/movies/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   baseUrl: 'https://api.themoviedb.org/',
-  imageBaseUrl: 'https://image.tmdb.org/t/p/',
   apiKey: '<YOUR_API_KEY_HERE>',
-  authRedirectUrl: 'https://www.themoviedb.org/authenticate'
-}
+  authRedirectUrl: 'https://www.themoviedb.org/authenticate',
+};
