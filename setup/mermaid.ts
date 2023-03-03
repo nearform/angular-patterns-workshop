0cc3e6ecffc2c8f24bfeb6d4637815a7b66cb2ba
Ian Lovell <102812633+ianlnf@users.noreply.github.com>
2023-03-03 10:13:17 +0000

    fix: add mermaid styling (#77)

---
+++ setup/mermaid.ts
@@ -0,0 +1,12 @@
+import { defineMermaidSetup } from '@slidev/types'
+
+export default defineMermaidSetup(() => {
+  return {
+    themeVariables: {
+      mainBkg: '#fff',
+      nodeBorder: '#fff',
+      edgeLabelBackground: '#fb7a9c',
+      textColor: '#000'
+    }
+  }
+})
